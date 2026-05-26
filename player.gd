513e3e531dc611cfdca525aa8443c92a9b6c6ae0
SoanGlengan <nongag.lesna@gmail.com>
2026-05-26 10:00:35 -0700

    add a broken health bar

--- player.gd
+++ player.gd
@@ -4,9 +4,10 @@ var ammo = 16
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var timeSinceLastShoot = 0
+var health = 100
 
-@onready var neck := $Neck
-@onready var camera := $Neck/Camera3D
+
+@onready var camera := $Camera3D
 
 
 
@@ -28,15 +29,11 @@ func _unhandled_input(event: InputEvent) -> void:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 			if event is InputEventMouseMotion:
-				neck.rotate_y(-event.relative.x * 0.001)
+				rotate_y(-event.relative.x * 0.001)
 				camera.rotate_x(-event.relative.y * 0.001)
 				camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(90))
-		if Input.is_action_pressed("shoot") and timeSinceLastShoot > 15:
-			shoot_bullet()
-			timeSinceLastShoot = 0
-		if Input.is_action_just_pressed("exit"):
-			$"../".exit_game(name.to_int())
-			get_tree().quit()
+				
+		
 func _physics_process(delta: float) -> void:
 	if is_multiplayer_authority():
 		if not is_on_floor():
@@ -51,13 +48,21 @@ func _physics_process(delta: float) -> void:
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var input_dir := Input.get_vector("left", "right", "forward", "back")
-		var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 		if direction:
 			velocity.x = direction.x * SPEED
 			velocity.z = direction.z * SPEED
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			velocity.z = move_toward(velocity.z, 0, SPEED)
+			
+		
+		if Input.is_action_pressed("shoot") and timeSinceLastShoot > 15:
+			shoot_bullet()
+			timeSinceLastShoot = 0
+		if Input.is_action_just_pressed("exit"):
+			$"../".exit_game(name.to_int())
+			get_tree().quit()
 		
 
 		move_and_slide()
@@ -66,8 +71,8 @@ func _physics_process(delta: float) -> void:
 func shoot_bullet():
 	const BULLET_3D = preload("res://bullet_3d.tscn")
 	var new_bullet = BULLET_3D.instantiate()
-	new_bullet.position = $Neck/Camera3D/Marker3D.global_position
-	new_bullet.rotation = $Neck/Camera3D/Marker3D.global_rotation
+	new_bullet.position = $Camera3D/Marker3D.global_position
+	new_bullet.rotation = $Camera3D/Marker3D.global_rotation
 	add_child(new_bullet)
 	ammo -= 1
 	
